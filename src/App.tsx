3f392b0f7071150cb272db57ee736c9c909c78dd
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2022-10-27 16:30:21 +0300

    add base model files, style and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { BoardComponent } from "./components/BoardComponent";
+import { Board } from "./models/Board";
 
-function App() {
+const App = () => {
+  const [board, setBoard] = useState(new Board());
+
+  useEffect(() => {
+    restart();
+  }, []);
+
+  function restart() {
+    const newBoard = new Board();
+    newBoard.initCells();
+    setBoard(newBoard);
+  }
   return (
-    <div className="App">
-      <header className="App-header">It work's</header>
+    <div className="app">
+      <BoardComponent board={board} setBoard={setBoard} />
     </div>
   );
-}
+};
 
 export default App;
