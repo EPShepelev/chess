fc8ac2107d8458786ef0e3cf2ad66bc3ceb6f5e7
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2022-10-28 15:53:08 +0300

    complete board and figures placeing

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -6,5 +6,9 @@ interface CellProps {
 }
 
 export const CellComponent: FC<CellProps> = ({ cell }) => {
-  return <div className={["cell", cell.color].join(" ")}></div>;
+  return (
+    <div className={["cell", cell.color].join(" ")}>
+      {cell.figure?.logo && <img src={cell.figure?.logo} alt="chess figure" />}
+    </div>
+  );
 };
